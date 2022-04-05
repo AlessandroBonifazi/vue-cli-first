ad6e559320b3453120a27395e20d0f88bf55bb6d
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-05 16:00:41 +0200

    adding component and prettier fix

---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+/* eslint-disable */
+<template>
+  <div>
+    <h2>La mia prima app con Vue CLI!</h2>
+  </div>
+</template>
+
+/* eslint-disable */
+<script>
+export default {
+  name: "Title-new",
+};
+</script>
